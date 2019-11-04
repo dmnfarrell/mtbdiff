9dc76728cdb8ffa9f1ad5b06b1b7c3f52fe29f12
Damien Farrell <farrell.damien@gmail.com>
2019-11-04 21:29:39 +0000

    orygis notebook

--- orygis_project/tree.R
+++ orygis_project/tree.R
@@ -1,9 +1,9 @@
   
 library("ape")
 library("phytools")
-setwd('~/gitprojects/mtbdiff')
+setwd('~/gitprojects/mtbdiff/orygis_project/')
 anim <- read.table('ani_matrix.csv',sep=',',header=TRUE, row.names=1)
-labels <- read.table('notebooks/orygis_samples.csv',sep=',',header=TRUE,row.names=1)
+samples <- read.table('orygis_samples.csv',sep=',',header=TRUE,row.names=1)
 #rownames(anim) <- anim$query
 
 distances <- as.matrix(1 - (anim/100))
@@ -11,9 +11,8 @@ dm <- dist(anim)
 njtree  <- nj(distances)
 rooted <- root(njtree,"MTB_beijing")
 rooted$edge.length[rooted$edge.length<0] <- 0
-labels <- labels[rooted$tip.label,]
-cols<-setNames(c('red','blue','white'),levels(as.factor(labels)))
-
+labels <- samples[rooted$tip.label,]$host
+cols<-setNames(c('red','blue','green2','white'),levels(as.factor(labels)))
 png('orygis_tree.png',width=800,height=800)
 plot(rooted, cex=2.5,label.offset=.00005)
 tiplabels(pie=to.matrix(labels, levels(labels)),cex=0.3,piecol=cols)
